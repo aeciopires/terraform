8ae08405a7eb7b9c687c6b643b4fb30166461a5a
Aecio Pires <aeciopires@gmail.com>
2018-10-15 17:37:09 -0300

    Ajuste no script de instalacao do salt

--- docker-zabbix/provisioning_with_salt.sh
+++ docker-zabbix/provisioning_with_salt.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+###########
+# Variables
+###########
+SALT=/usr/bin/salt
+
+######
+# MAIN
+######
+
 # Install SaltStack
 # Only Ubuntu 18.04
 # wget -O - https://repo.saltstack.com/apt/ubuntu/18.04/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add -
-#
-# curl -L https://bootstrap.saltstack.com -o /tmp/install_salt.sh;
-# sudo sh /tmp/install_salt.sh -P -M;
+if [ ! -f $SALT ] ; then
+  echo "[WARNING] Nao encontrei o(s) arquivo(s) \"$SALT\""
+  echo "Tentarei instalar..."
+  curl -L https://bootstrap.saltstack.com -o /tmp/install_salt.sh;
+  sudo sh /tmp/install_salt.sh -P -M;
+  if [ ! -f $SALT ] ; then
+    echo "[ERROR] Nao encontrei o(s) arquivo(s) \"$SALT\""
+    exit 4
+  fi
+fi
 
 # Copies files
 sudo mkdir -p /srv
