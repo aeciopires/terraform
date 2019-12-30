aafdfe1750af683629fb196a1f1726f05c72bcea
Aecio Pires <aeciopires@gmail.com>
2019-12-30 08:29:53 -0300

    Adjust double cotes

--- aws_docker_openproject/scripts/start_openproject.sh
+++ aws_docker_openproject/scripts/start_openproject.sh
@@ -5,12 +5,12 @@ DIR_LOGS=$2
 DIR_STATIC=$3
 SECRET=$4
 
-sudo mkdir -p $DIR_PGDATA
-sudo mkdir -p $DIR_LOGS
-sudo mkdir -p $DIR_STATIC
+sudo mkdir -p "$DIR_PGDATA"
+sudo mkdir -p "$DIR_LOGS"
+sudo mkdir -p "$DIR_STATIC"
 
-sudo docker run -d -p 80:80 --name openproject -e SECRET_KEY_BASE=$SECRET \
-  -v $DIR_PGDATA:/var/lib/postgresql/9.6/main \
-  -v $DIR_LOGS:/var/log/supervisor \
-  -v $DIR_STATIC:/var/db/openproject \
+sudo docker run -d -p 80:80 --name openproject -e SECRET_KEY_BASE="$SECRET" \
+  -v "$DIR_PGDATA":/var/lib/postgresql/9.6/main \
+  -v "$DIR_LOGS":/var/log/supervisor \
+  -v "$DIR_STATIC":/var/db/openproject \
   openproject/community:8
